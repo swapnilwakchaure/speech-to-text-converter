de805b64affb9c3163e729e28e7ee012d3f6b655
swapnilwakchaure <swapnilwakchaure2411999@gmail.com>
2025-02-03 21:59:15 +0530

    [build]: deployment issue fixes

--- dist/index.js
+++ dist/index.js
@@ -19,7 +19,7 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const db_1 = require("./config/db");
 const index_1 = __importDefault(require("./routes/index"));
 dotenv_1.default.config();
-const port = process.env.port || 3030;
+const port = process.env.PORT || 3030;
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
